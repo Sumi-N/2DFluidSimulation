e7ead19f39fc6c58db879eb6206b5d47cfb8e824
Sumi <sumi.n.lcy@gmail.com>
2020-04-21 13:06:10 -0600

    Initial commit2

---
+++ 2DFluidSimulation/Source.cpp
@@ -0,0 +1,65 @@
+#include <cstdlib>
+#include <iostream>
+
+#include <GL/glew.h>
+#include <GLFW/glfw3.h>
+
+#define WIDTH 1920
+#define HEIGHT 1080
+
+GLFWwindow* window;
+
+int main()
+{
+	// Initialize GLFW
+	if (glfwInit() == GL_FALSE)
+	{
+		// If glfw initialization failed
+		std::cerr << "Can't initialize GLFW" << std::endl;
+		return 0;
+	}
+
+	// Register a function which will execute when the program finish 
+	atexit(glfwTerminate);
+
+	//Select OpenGL Version
+	glfwWindowHint(GLFW_CONTEXT_VERSION_MAJOR, 4);
+	glfwWindowHint(GLFW_CONTEXT_VERSION_MINOR, 3);
+	glfwWindowHint(GLFW_OPENGL_FORWARD_COMPAT, GL_TRUE);
+	glfwWindowHint(GLFW_OPENGL_PROFILE, GLFW_OPENGL_CORE_PROFILE);
+
+	// Creating a window
+	window = glfwCreateWindow(WIDTH, HEIGHT, "Interactive Graphics", NULL, NULL);
+	if (window == NULL)
+	{
+		// If the window is not created
+		std::cerr << "Can't create GLFW window." << std::endl;
+		glfwTerminate();
+		return 0;
+	}
+	// Attached the OpenGL to this window
+	glfwMakeContextCurrent(window);
+
+	// Initialize GLEW
+	glewExperimental = GL_TRUE;
+	if (glewInit() != GLEW_OK)
+	{
+		std::cerr << "Can't initialize GLEW" << std::endl;
+		return 0;
+	}
+
+	// The timing to wait for V-Sync
+	glfwSwapInterval(1);
+
+	// Set background color
+	glClearColor(0.0f, 0.0f, 0.0f, 0.0f);
+
+	while (glfwWindowShouldClose(window) == GL_FALSE)
+	{
+		glfwPollEvents();
+
+		glfwSwapBuffers(window);
+	}
+
+	return 0;
+}
