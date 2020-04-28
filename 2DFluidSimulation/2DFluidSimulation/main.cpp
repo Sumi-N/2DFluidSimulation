d4eaafeb9f13ffe5be0255ebff313e034b10e8b4
Sumi <sumi.n.lcy@gmail.com>
2020-04-28 15:13:20 -0600

    Done for now

--- 2DFluidSimulation/2DFluidSimulation/main.cpp
+++ 2DFluidSimulation/2DFluidSimulation/main.cpp
@@ -187,7 +187,7 @@ int main()
 
 	// Load graphic texture
 	TextureAttribute* graphctexture = new TextureAttribute();
-	graphctexture->Load("../../Assets/Textures/texture.png");
+	graphctexture->Load("../../Assets/Textures/thumb-1920-692072.png");
 
 	// Create graphic texture
 	GLuint graphctextureid;
